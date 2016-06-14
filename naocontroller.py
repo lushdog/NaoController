d6691d5c7a19cdbbda876159c751fbc785202851
Matt Moore <matt.e.moore@gmail.com>
2016-06-14 17:09:04 -0500

    Sit and stand support plus tests

--- naocontroller.py
+++ naocontroller.py
@@ -9,17 +9,35 @@ from naoqi import ALProxy
 class NaoController:
 
     def connect_to_robot(self):
-        proxy = None
+        self.tts_proxy = None
+        self.motion_proxy = None
+        self.posture_proxy = None
+
+        print "Connecting to robot..."
         try:
-            proxy = ALProxy("ALAnimatedSpeech", self.ip, self.port)
-            print "Connected to robot..."
-        except RuntimeError as e:
-            print "Error connecting to robot({0})".format(e)
-        return proxy
+            self.tts_proxy = ALProxy("ALAnimatedSpeech", self.ip, self.port)
+        except Exception, e:
+            print "Could not create proxy to ALAnimatedSpeech"
+            print "Error was: ", e
+
+        if (self.tts_proxy):
+            try:
+                self.motion_proxy = ALProxy("ALMotion", self.ip, self.port)
+            except Exception, e:
+                print "Could not create proxy to ALMotion"
+                print "Error was: ", e
+
+        if (self.motion_proxy):
+            try:
+                self.posture_proxy = ALProxy("ALRobotPosture", self.ip, self.port)
+            except Exception, e:
+                print "Could not create proxy to ALRobotPosture"
+                print "Error was: ", e
 
     def print_usage(self):
-        print 'Command format:"Text to say" "Animation tag while text is playing"'
+        print 'Command format:"Text to say" "Animation tag while text is playing" or PostureToAssume'
         print 'Example: "Hello, how are you" "Bow"'
+        print 'Example: Sit'
         print 'Quit by typing "exit" (without quotes)'
 
     def command_loop(self, proxy):
@@ -39,21 +57,50 @@ class NaoController:
 
     @staticmethod
     def parse_command(command):
-        match_pattern = '^"([^"]*)"\s+"([A-Za-z]*)"$'
+        match_pattern = '^"([^"]*)"\s+"([A-Za-z]*)"$|^(?i)(Stand|Sit)$'
         match = re.match(match_pattern, command)
         if(match):
             speech = match.group(1)
             animation = match.group(2)
-            return(speech, animation)
+            posture = match.group(3)
+            return(speech, animation, posture)
         return None
 
-    def invoke_command(self, proxy, speech, animation,):
+    def invoke_command(self, speech, animation, posture):
+        "Sending command to Nao..."
+        if (speech):
+            self.invoke_speech(speech, animation)      
+        else:
+            self.invoke_posture(posture)
+            
+    def invoke_speech(self, speech, animation):
         animatedSpeech = '^startTag({0}) "{1}" ^waitTag({0})'.format(animation, speech)
-        proxy.say(animatedSpeech)
+        self.tts_proxy.say(animatedSpeech)
 
+    def invoke_posture(self, posture):
+        posture = string.lower(posture)
+        if (posture == 'sit'):
+            self.invoke_sit()
+        elif (posture == 'stand'):
+            self.invoke_stand()
+
+    def invoke_stand(self):
+        self.set_body_stiffness(1.0)
+        self.set_pose('StandInit')
+
+    def invoke_sit(self):
+        self.set_pose('Sit')
+        self.set_body_stiffness(0.0)
+
+    def set_body_stiffness(stiffness):
+        self.motion_proxy.stiffnessInterpolation("Body", stiffness, stiffness) 
+
+    def set_pose(pose):
+        self.posture_proxy.goToPosture(pose, 0.5)
+    
     def main(self):
-        proxy = self.connect_to_robot()
-        if (proxy):
+        self.connect_to_robot()
+        if (self.tts_proxy and self.motion_proxy and self.posture_proxy):
             self.print_usage()
             self.command_loop(proxy)
 
--- naocontrollertests.py
+++ naocontrollertests.py
@@ -7,17 +7,23 @@ import naocontroller as nc
 
 class NaoControllerTests(unittest.TestCase):
 
-    def test_connection_bad_ip_returns_none(self):
+    def test_connection_bad_ip_returns_all_proxies_none(self):
         robot = nc.NaoController()
         robot.ip = '$$$'
         robot.port = 9559
-        self.assertIsNone(robot.connect_to_robot())
+        robot.connect_to_robot()
+        self.assertIsNone(robot.tts_proxy)
+        self.assertIsNone(robot.motion_proxy)
+        self.assertIsNone(robot.posture_proxy)
 
     def test_connection_bad_port_returns_none(self):
         robot = nc.NaoController()
         robot.ip = '192.168.1.1'
         robot.port = '@@@'
-        self.assertIsNone(robot.connect_to_robot())
+        robot.connect_to_robot()
+        self.assertIsNone(robot.tts_proxy)
+        self.assertIsNone(robot.motion_proxy)
+        self.assertIsNone(robot.posture_proxy)
     
     def test_command_loop_first_input_is_exit_does_exit(self):
         robot = nc.NaoController()
@@ -31,23 +37,69 @@ class NaoControllerTests(unittest.TestCase):
         robot.parse_command = mock.Mock(return_value=[None])
         robot.invoke_command = mock.Mock()
         robot.command_loop(None)
-        self.assertEqual(robot.parse_command.call_count, 1)  
+        self.assertEqual(robot.parse_command.call_count, 1)
+        self.assertEqual(robot.invoke_command.call_count, 1)
 
+    def test_invoke_command_with_speech_calls_invoke_speech(self):
+        robot = nc.NaoController()
+        robot.invoke_speech = mock.Mock()
+        robot.invoke_posture = mock.Mock()
+        robot.invoke_command("Speech", "Animation", None);
+        robot.invoke_speech.assert_called_once_with("Speech", "Animation")
+        self.assertEqual(robot.invoke_posture.call_count, 0)
+
+    def test_invoke_command_with_posture_calls_invoke_posture(self):
+        robot = nc.NaoController()
+        robot.invoke_speech = mock.Mock()
+        robot.invoke_posture = mock.Mock()
+        robot.invoke_command(None, None, "Posture");
+        robot.invoke_posture.assert_called_once_with("Posture")
+        self.assertEqual(robot.invoke_speech.call_count, 0)
+
+    def test_invoke_posture_with_sit_calls_invoke_sit(self):
+        robot = nc.NaoController()
+        robot.invoke_stand = mock.Mock()
+        robot.invoke_sit = mock.Mock()
+        robot.invoke_posture('sit')
+        robot.invoke_sit.assert_called_once
+        self.assertEqual(robot.invoke_stand.call_count, 0)
+
+    def test_invoke_posture_with_stand_calls_invoke_stand(self):
+        robot = nc.NaoController()
+        robot.invoke_stand = mock.Mock()
+        robot.invoke_sit = mock.Mock()
+        robot.invoke_posture('stand')
+        robot.invoke_stand.assert_called_once
+        self.assertEqual(robot.invoke_sit.call_count, 0)
+
+    def test_invoke_posture_with_unknown_posture_calls_nothing(self):
+        robot = nc.NaoController()
+        robot.invoke_stand = mock.Mock()
+        robot.invoke_sit = mock.Mock()
+        robot.invoke_posture('unkownposture')
+        self.assertEqual(robot.invoke_stand.call_count, 0)
+        self.assertEqual(robot.invoke_sit.call_count, 0)
     
     #test command_loop (non_exit_input with good date invokes command, non_exit_input with bad data does not invoke command)
         
     #regex tests
     def test_parse_command_empty(self):
         self.assertIsNone(nc.NaoController.parse_command(''))
-    def test_parse_command_simple_valid_command_returns_valid(self):
-        self.assertEqual(nc.NaoController.parse_command('"Speech" "Animation"'), ("Speech","Animation"))
-    def test_parse_command_simple_invalid_command_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('dfasdfasdfasdf'))
+    def test_parse_command_simple_speech_animation_returns_valid(self):
+        self.assertEqual(nc.NaoController.parse_command('"Speech" "Animation"'), ("Speech","Animation", None))
+    def test_parse_command_stand_posture_returns_valid(self):
+        self.assertEqual(nc.NaoController.parse_command('Stand'), (None, None, 'Stand'))
+    def test_parse_command_sit_posture_returns_valid(self):
+        self.assertEqual(nc.NaoController.parse_command('Sit'), (None, None, 'Sit'))
+    def test_parse_command_unsupported_posture_returns_invalid(self):
+        self.assertIsNone(nc.NaoController.parse_command('UnsupportedPosture'))
+    def test_parse_command_posture_with_spaces_returns_invalid(self):
+        self.assertIsNone(nc.NaoController.parse_command('Posture Space'))
     def test_parse_command_too_many_quotes_command_returns_none(self):
         self.assertIsNone(nc.NaoController.parse_command('"Spee"ch" "Animation"'))
     def test_parse_command_missing_animation_command_returns_none(self):
         self.assertIsNone(nc.NaoController.parse_command('"Speech"'))
-    def test_parse_command_no_space_command_returns_valid(self):
+    def test_parse_command_no_space_command_returns_none(self):
         self.assertIsNone(nc.NaoController.parse_command('"Speech""Animation"'))
     def test_parse_command_trailing_command_returns_none(self):
         self.assertIsNone(nc.NaoController.parse_command('"Speech" "Animation"dfadfasdf'))
