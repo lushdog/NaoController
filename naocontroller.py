715b04ca5f97e4f79a6756bc2ccf941af83c17ad
Matt Moore <matt.e.moore@gmail.com>
2016-07-14 17:42:21 -0500

    Initial refactor to support CMD2
    Supports cmd and cmd2 module
    Refactor of do_*, invoke_*, set_*
    Initial implementation of move_toward

--- naocontroller.py
+++ naocontroller.py
@@ -1,12 +1,9 @@
-import argparse
-import sys
-import string
-import re
-
+import argparse, sys, string, re, os, math
 import defaults
 from naoqi import ALProxy
+import cmd2 as cmd
 
-class NaoController:
+class NaoController(cmd.Cmd):
 
     ANIM_SPEECH_LIB = "ALAnimatedSpeech"
     MOTION_LIB = "ALMotion"
@@ -17,117 +14,165 @@ class NaoController:
     PROXY_LIBS = [ANIM_SPEECH_LIB, MOTION_LIB, POSTURE_LIB,
                 BASIC_AWARE_LIB, AUTONOMOUS_MOVES_LIB, AUTONOMOUS_LIFE_LIB]
 
-    def connect_to_robot(self):
-        print "Connecting to robot..."
+    is_connected = False
+    intro = 'Welcome to the Nao controller.   Type help or ? to list commands.'
+    prompt = '(nao)'
+
+    '''COMMAND PROCESSORS'''
+    def do_connect(self, arg):
+        '''Connect to nao robot via 'connect <ip/host of robot> <port>' or 'connect' alone (uses defaults.py)'''
+        if (self.is_connected):
+            self.stdout.write('There is already a connection to a robot.\n')
+            return
+        self.invoke_connect(arg)
+                 
+    def do_say(self, args):
+        '''Robot will say a line of text and play an animation.'''
+        if (not self.is_connected):
+            self.print_not_connected_error()
+            return
+        self.invoke_say(*self.get_say_inputs())
+
+    def do_stand(self, arg):
+        '''Robot will stand and start breathing.'''
+        if (not self.is_connected):
+            self.print_not_connected_error()
+            return
+        self.invoke_stand()
+
+    def do_sit(self, arg):
+        '''Robot will sit and stop breathing.'''
+        if (not self.is_connected):
+            self.print_not_connected_error()
+            return
+        self.invoke_sit()
+
+    def do_autolife(self, arg):
+        '''Toggles autonomous life state of the robot.'''
+        if (not self.is_connected):
+            self.print_not_connected_error()
+            return
+        self.invoke_toggle_autolife()
+
+    def do_move(self, arg):
+        '''Moves the robot'''
+        if (not self.is_connected):
+            self.print_not_connected_error()
+            return
+        self.invoke_move_toward(arg)
+        
+    def do_EOF(self, line):
+        return True
+
+
+    '''COMMAND INVOCATION'''
+    def invoke_connect(self, arg):
+        if (len(arg) > 1): #very weak param validation
+            ip = arg[0]
+            port = arg[1]
+        else:
+            ip = defaults.DEFAULT_IP
+            port = defaults.DEFAULT_PORT
+                    
+        self.stdout.write('Connecting to robot on {0}:{1}...\n'.format(ip, port))
         self.robot_proxies = {}
         for lib in self.PROXY_LIBS:
             try:
-                self.robot_proxies[lib] = ALProxy(lib, self.ip, self.port)
+                self.robot_proxies[lib] = ALProxy(lib, ip, port)
             except Exception, e:
-                print "Could not create proxy to ", lib
-                print "Error was: ", e
-                return False
+                self.stdout.write('\nCould not create proxy to {0}\n'.format(lib))
+                self.stdout.write('\nError was: {0}\n'.format(e))
+                self.is_connected = False
+                return
+        self.is_connected = True
         self.set_autonomous_life(False)
-        return True
 
-    def print_usage(self):
-        print 'Text to speech command:"Text to say" "Animation tag while text is playing"'
-        print 'Posture command: Posture'
-        print 'Subsytem command: SubsystemToToggle'
-        print 'Example: "Hello, how are you" "Bow"'
-        print 'Example" "That is not correct" "Incorrect"'
-        print 'Example: Sit'
-        print 'Example: Autolife'
-        print 'Quit by typing "exit" (without quotes)'
-
-    def command_loop(self):
-        command = self.get_command()
-        while (string.lower(command) != 'exit'):
-            parsed_command = NaoController.parse_command(command)
-            if (parsed_command):
-                #print parsed_command
-                self.invoke_command(*parsed_command)            
-            else:
-                print 'Command format was invalid'
-                self.print_usage()
-            command = self.get_command()
-
-    def get_command(self):
-        return raw_input("Command:")
-
-    @staticmethod
-    def parse_command(command):
-        match_pattern = '^\s*"([^"\\\\]*)"\s+"([A-Za-z]*)"\s*$|^\s*(?i)(Stand|Sit|Autolife)\s*$'
-        match = re.match(match_pattern, command)
-        if(match):
-            speech = match.group(1)
-            animation = match.group(2)
-            method = match.group(3)
-            return(speech, animation, method)
-        return None
-
-    @staticmethod
-    def print_sub_system_update(set_on, sub_process):
-         print "Turning {on_off} {sub_process}...".format(on_off = 'on' if set_on == True else 'off', sub_process = sub_process)
-  
-    def invoke_command(self, speech, animation, method):
-        "Sending command to Nao..."
-        if (speech):
-            self.invoke_speech(speech, animation)      
-        else:
-            self.invoke_method(method)
-            
-    def invoke_speech(self, speech, animation):
-        animatedSpeech = '^startTag({0}) "\\rspd={2}\\{1}" ^waitTag({0})'.format(animation.lower(), speech, defaults.SPEECH_SPEED)
+    def invoke_say(self, animation, speech):
+        cleaned_animation = self.clean_animation(animation)
+        cleaned_speech = self.clean_speech(speech)
+        animatedSpeech = self.format_say_inputs(cleaned_animation, cleaned_speech)
         self.robot_proxies[self.ANIM_SPEECH_LIB].say(animatedSpeech)
 
-    def invoke_method(self, method):
-        if (method.lower() == 'sit'):
-            self.invoke_sit()
-        elif(method.lower() == 'stand'):
-            self.invoke_stand()
-        elif(method.lower() == 'autolife'):
-            self.toggle_auto_life()
+    #MM:do we need to strip any unsupported characters from animation and speech?
+    def clean_speech(self, speech):
+        return speech
+
+    def clean_animation(self, animation):
+        return animation
+
+    def format_say_inputs(self, animation, speech):
+        animatedSpeech = '^startTag({0}) "\\rspd={2}\\{1}" ^waitTag({0})'.format(animation.lower(), speech, defaults.SPEECH_SPEED)
+        return animatedSpeech
+
+    def get_say_inputs(self):
+        self.stdout.write('What would you like the robot to say?\n')
+        speech = self.stdin.readline()
+        self.stdout.write('What animation would you like the robot to perform?\n')
+        animation = self.stdin.readline()
+        return animation, speech
 
     def invoke_stand(self):
-        print 'Standing...'
+        self.stdout.write('Standing...')
         self.set_body_stiffness(1.0)
         self.set_pose('Stand')
         self.set_breathing(True)
-        is_alive = self.get_autonomous_life()
         
     def invoke_sit(self):
-        print 'Sitting...'
+        self.stdout.write('Sitting...')
         self.set_breathing(False)
         self.set_pose('Sit')
-        self.set_body_stiffness(0.0)
-        
+        self.set_body_stiffness(0.0)  
+
     def set_body_stiffness(self, stiffness):
-        print 'Setting stiffness to {stiffness}...'.format(stiffness = stiffness)
+        self.stdout.write('Setting stiffness to {0}...'.format(stiffness))
         self.robot_proxies[self.MOTION_LIB].stiffnessInterpolation("Body", stiffness, 1.0) 
 
     def set_pose(self, pose):
         self.robot_proxies[self.POSTURE_LIB].goToPosture(pose, 0.5)
-        
-    def toggle_auto_life(self):
+
+    def invoke_move_to(self, arg):
+        processed_args = list(*self_process_move_arg(arg))
+        if (processed_args[0] == False):
+            self.stdout.write("Invalid arguments passed to move_to command.")
+            return
+        motion = self.robot_proxies[self.MOTION_LIB]
+        motion.setMotionConfig([["ENABLE_FOOT_CONTACT_PROTECTION", False]])
+        motion.setMoveArmsEnabled(True, True)
+        motion.moveTo(processed_args[1], processed_y[2], processed_theta[3])
+        #MM: enable collision detection
+
+    def process_move_arg(self, arg):
+        ##MM: if not 3 args 
+        ##MM: if non-number args
+        ##MM: if degress less than 0 and bigger than 360 clamp
+        ##MM: unit test process and validation
+        x = arg[0]
+        y = arg[1]
+        theta = math.radians(arg[2])
+        return True, x, y, theta
+
+    def invoke_toggle_autolife(self):
         current_state = (self.robot_proxies[self.AUTONOMOUS_LIFE_LIB].getState() == 'solitary')
-        self.set_autonomous_life(not current_state)
-        
+        if (current_state == 'interactive'):
+            self.stdout.write('Cannot change autolife state when in interactive mode.')
+        else:
+            self.set_autonomous_life(not current_state)
+
     def set_autonomous_life(self, set_on):
-        NaoController.print_sub_system_update(set_on, 'Autonomous Life')
+        self.print_sub_system_update(set_on, 'Autonomous Life')
         target_state = 'solitary' if set_on else 'disabled' #todo: this causes exception if the robot is in interactive state
         self.robot_proxies[self.AUTONOMOUS_LIFE_LIB].setState(target_state)
         
     def get_autonomous_life(self):
         return (self.robot_proxies[self.AUTONOMOUS_LIFE_LIB].getState() != 'disabled')
-            
+  
     def set_autonomous_moves(self, set_on):
-        NaoController.print_sub_system_update(set_on, 'Autonomous Moves')
+        self.print_sub_system_update(set_on, 'Autonomous Moves')
         target_state = 'backToNeutral' if set_on else 'none'
         self.robot_proxies[self.AUTONOMOUS_MOVES_LIB].setBackgroundStrategy(target_state)
         
     def set_awareness(self, set_on):
-        NaoController.print_sub_system_update(set_on, 'Basic Awareness')
+        self.print_sub_system_update(set_on, 'Basic Awareness')
         proxy = self.robot_proxies[self.BASIC_AWARE_LIB]
         proxy.startAwareness() if set_on else proxy.stopAwareness()
 
@@ -136,25 +181,32 @@ class NaoController:
         self.robot_proxies[self.MOTION_LIB].setBreathEnabled('Body', set_on)
         NaoController.print_sub_system_update(set_on, 'arm breathing')
         self.robot_proxies[self.MOTION_LIB].setBreathEnabled('Arms', set_on)
-  
-    def main(self):
-        if (self.connect_to_robot()):
-            self.print_usage()
-            self.command_loop()
 
-    def __init__(self):
-        self.ip = None
-        self.port = None
+    def print_not_connected_error(self):
+        self.stdout.write('You cannot run this command until you connect to robot with the CONNECT command\n')
+
+    def print_sub_system_update(self, set_on, sub_process):
+        on_off = ['off', 'on']
+        self.stdout.write('Turning {0} {1}...'.format(on_off[set_on], sub_process))
+  
+    #def main(self):
+
+    def __init__(self, script):
+        if (script is not None):
+            cmd.Cmd.__init__(self, stdin=script)
+            self.use_rawinput = False
+        else:
+            cmd.Cmd.__init__(self)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Connect to and start interactive session to send talk and animation commands to Nao')
-    parser.add_argument('-ip', default=defaults.DEFAULT_IP, dest='ip', help='IP (or hostname) of your Nao robot.')
-    parser.add_argument('-port', default=defaults.DEFAULT_PORT, dest='port', help='Port of your Nao robot.')
-    args = parser.parse_args()
-    nc = NaoController()
-    nc.ip = args.ip
-    nc.port = args.port
-    nc.main()
+    if (len(sys.argv) > 1):
+        script = open(sys.argv[1], 'rt')
+        try:
+            NaoController(script).cmdloop()
+        finally:
+            script.close()
+    else:
+        NaoController(None).cmdloop()
 
 
     
--- naocontrollertests.py
+++ naocontrollertests.py
@@ -2,50 +2,47 @@ import unittest
 import mock
 import string
 import random
+import defaults
+
 from mock import patch
 
 import naocontroller as nc
 
 class NaoControllerTests(unittest.TestCase):
 
-    def test_connection_bad_ip_returns_all_proxies_none(self):
-        print 'Testing bad ip...'
-        robot = nc.NaoController()
-        robot.ip = '$$$'
-        robot.port = 9559
-        self.assertEqual(robot.connect_to_robot(), False)
+    def test_do_connect_with_bad_ip_sets_is_connected_false(self):
+        robot = nc.NaoController(None)
+        robot.do_connect(['$$$', 9559])
+        self.assertEqual(robot.is_connected, False)
 
-    def test_connection_bad_port_returns_none(self):
-        print 'Testing bad port...'
-        robot = nc.NaoController()
-        robot.ip = '192.168.1.1'
-        robot.port = '@@@'
-        self.assertEqual(robot.connect_to_robot(), False)
-    
-    def test_command_loop_first_input_is_exit_does_exit(self):
-        robot = nc.NaoController()
-        robot.get_command = mock.Mock(side_effect=['exit', 'foo'])
-        robot.command_loop()
-        self.assertEqual(robot.get_command.call_count, 1)
+    def test_do_connect_with_bad_port_sets_is_connected_false(self):
+        robot = nc.NaoController(None)
+        robot.do_connect(['192.168.1',9559])
+        self.assertEqual(robot.is_connected, False)
 
-    def test_command_loop_second_input_is_exit_loop_then_exit(self):
-        robot = nc.NaoController()
-        with patch.object(nc.NaoController, "parse_command") as mocked_parse_command:
-            robot.get_command = mock.Mock(side_effect=['foo', 'exit'])
-            mocked_parse_command.side_effect = [None]
-            robot.invoke_command = mock.Mock()
-            robot.command_loop()
-            self.assertEqual(nc.NaoController.parse_command.call_count, 1)
-            self.assertEqual(robot.invoke_command.call_count, 0)
+    def mock_speech_proxy(self):
+        robot = nc.NaoController(None)
+        robot.is_connected = True
+        robot.robot_proxies = {};
+        robot.robot_proxies[robot.ANIM_SPEECH_LIB] = mock.Mock()
+        robot.robot_proxies[robot.ANIM_SPEECH_LIB].say = mock.Mock()
+        return robot
+        
+    def test_do_say_with_valid_args_invokes_say(self):
+        robot = self.mock_speech_proxy()
+        robot.get_say_inputs = mock.Mock(return_value=('foo', 'bar'))
+        robot.do_say(None)
+        robot.robot_proxies[robot.ANIM_SPEECH_LIB].say.assert_called_once_with('^startTag(foo) "\\rspd={0}\\bar" ^waitTag(foo)'.format(defaults.SPEECH_SPEED))
 
-    def test_invoke_command_with_speech_calls_invoke_speech(self):
-        robot = nc.NaoController()
-        robot.invoke_speech = mock.Mock()
-        robot.invoke_method = mock.Mock()
-        robot.invoke_command("Speech", "Animation", None);
-        robot.invoke_speech.assert_called_once_with("Speech", "Animation")
-        self.assertEqual(robot.invoke_method.call_count, 0)
+    '''
+    def test_do_say_with_invalid_args(self):
+        robot = self.mock_speech_proxy()
+        robot.get_say_inputs = mock.Mock(return_value=('fo\"o', 'bar'))
+        robot.do_say(None)
+        robot.robot_proxies[robot.ANIM_SPEECH_LIB].say.assert_called_once_with('^startTag(fo\'o) "\\rspd={0}\\bar" ^waitTag(fo\'o)'.format(defaults.SPEECH_SPEED))
+    '''
 
+    '''
     def test_invoke_command_with_posture_calls_invoke_posture(self):
         robot = nc.NaoController()
         robot.invoke_speech = mock.Mock()
@@ -83,44 +80,7 @@ class NaoControllerTests(unittest.TestCase):
         robot.toggle_auto_life = mock.Mock()
         robot.invoke_method('autolife')
         self.assertEqual(robot.toggle_auto_life.call_count, 1)
-    
-    #test command_loop (non_exit_input with good date invokes command, non_exit_input with bad data does not invoke command)
-        
-    #regex tests
-    def test_parse_command_empty(self):
-        self.assertIsNone(nc.NaoController.parse_command(''))
-    def test_parse_command_simple_speech_animation_returns_valid(self):
-        self.assertEqual(nc.NaoController.parse_command('"Speech" "Animation"'), ("Speech","Animation", None))
-    def test_parse_command_stand_posture_returns_valid(self):
-        self.assertEqual(nc.NaoController.parse_command('Stand'), (None, None, 'Stand'))
-    def test_parse_command_sit_posture_returns_valid(self):
-        self.assertEqual(nc.NaoController.parse_command('Sit'), (None, None, 'Sit'))
-    def test_parse_command_autolife_in_speech_returns_valid(self):
-        self.assertEqual(nc.NaoController.parse_command('autolife'), (None, None, 'autolife'))
-    def test_parse_command_whitespaces_return_valid(self):
-        self.assertEqual(nc.NaoController.parse_command('    autolife   '), (None, None, 'autolife'))  
-    def test_parse_command_unsupported_posture_returns_invalid(self):
-        self.assertIsNone(nc.NaoController.parse_command('UnsupportedPosture'))
-    def test_parse_command_posture_with_spaces_returns_invalid(self):
-        self.assertIsNone(nc.NaoController.parse_command('Posture Space'))
-    def test_parse_command_too_many_quotes_command_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Spee"ch" "Animation"'))
-    def test_parse_command_missing_animation_command_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Speech"'))
-    def test_parse_command_no_space_command_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Speech""Animation"'))
-    def test_parse_command_trailing_command_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Speech" "Animation"dfadfasdf'))
-    def test_parse_command_invalid_chars_in_animation_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Speech" "Ani#m@tion"'))
-    def test_parse_command_quotes_in_speech_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Sp"eech" "Animation"'))
-    def test_parse_command_no_animation_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Speech Animation"'))
-    def test_parse_command_backslash_in_speech_returns_none(self):
-        self.assertIsNone(nc.NaoController.parse_command('"Spe\ech" "Animation"'))
-   
-   
-                     
+
+    '''    
 if __name__ == '__main__':
     unittest.main()
