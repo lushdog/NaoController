b26115df5c1ed4cf90d9e03542b317f9eb695cdf
Matt Moore <matt.e.moore@gmail.com>
2016-10-10 21:19:01 -0500

    Commands can be sent viea naoweb and console output from naocommandline is returned

--- examples/webinterface/naoweb.py
+++ examples/webinterface/naoweb.py
@@ -1,7 +1,6 @@
 import sys
 from cStringIO import StringIO 
 import bottle
-from bottle import request, route, SimpleTemplate, view, static_file, post
 from beaker.middleware import SessionMiddleware
 
 import naocontroller.examples.commandline.naocommandline as commandline
@@ -22,37 +21,72 @@ def setup_request():
     request.session = bottle.request.environ.get('beaker.session')
 '''
 
-@route('/static/<filename:path>')
+@bottle.route('/static/<filename:path>')
 def send_static(filename):
-    return static_file(filename, root='static')
+    return bottle.static_file(filename, root='static')
 
-@route('/')
-@view('naoweb')
+@bottle.route('/')
+@bottle.view('naoweb')
 def main():
-    return None
-
-@route('/connect')
-def connect():
     command_line = commandline.NaoCommandLine(None)
     command_line.use_rawinput = False
     _set_command_line(command_line)
-    return _invoke_command('connect ')
+    return None
 
-@post('/send')
-def send():
-    return _invoke_command(request.json)
+@bottle.post('/command')
+def run_command():
+    """Runs naocommandline command, returns console output and sets connected cookie"""
+    output = _invoke_command(bottle.request.json['command']) #pylint: disable=unsubscriptable-object
+    bottle.response.set_cookie("naoconnected", '1' if _get_command_line().is_connected else '0')
+    return output
 
+@bottle.route('/camera')
+def get_camera_image_top():
+    return get_camera_image()
+
+@bottle.route('/camera/<camera_name>')
+def get_camera_image(camera_name='top'):
+    """Takes a picture with top or bottom camera and returns the image contents in png format
+        encoded in base64. 
+    """
+    use_bottom_camera = (camera_name == 'bottom')
+    image = None
+    try:
+        video_controller = _get_command_line().video_controller
+        image = video_controller.get_picture(use_bottom_camera)
+    except (KeyError, AttributeError):
+        bottle.abort(400, "Must /connect first.")
+    except (ValueError, TypeError):
+        bottle.abort(400, "Invalid value sent to server.")
+    except Exception: # pylint: disable=broad-except
+        bottle.abort(500, "Error on server.")
+
+    if image is not None:
+        output = StringIO()
+        image.save(output, "PNG")
+        contents = output.getvalue().encode("base64")
+        output.close()
+        #contents = contents.split('\n')[0] #comment this if it doesn't work
+        return contents
+    else:
+        return None 
+    
 def _set_command_line(command_line):
-    bottle.request.environ.get('beaker.session')['commandline'] = command_line
+    session = bottle.request.environ.get('beaker.session')
+    session['commandline'] = command_line
+    #session.save()
 
 def _get_command_line():
     return bottle.request.environ.get('beaker.session')['commandline']
 
 def _invoke_command(command):
-    old_stdout = sys.stdout
-    sys.stdout = new_stdout = StringIO()
+    """Redirects 'console' output, invokes commandline-style command and returns console output
+        Note: NaoCommandLine catches most every exception and ouputs error messages to console
+        which is what naoweb does as well therefore no all invoked commands will return 200 OK."""
+    old_stdout = _get_command_line().stdout 
+    _get_command_line().stdout = new_stdout = StringIO()
     _get_command_line().onecmd(command)
-    sys.stdout = old_stdout
+    _get_command_line().stdout = old_stdout
     return new_stdout.getvalue()
 
 bottle.run(app=APP, host='localhost', port=8080, debug=True)
--- examples/webinterface/views/naoweb.tpl
+++ examples/webinterface/views/naoweb.tpl
@@ -10,26 +10,38 @@
 		<div id="camera" style="width:50%;height:70%;border:all=1px;border-style:solid;margin-left:50%"><img alt="Bottom Camera"></div></div>
 		<div id="output" style="width:100%;height:20%;border:all=1px;border-style:solid;display:inline-block;">
 			<div id="console">
-				<textarea id="consoletext" style="width:100%;max-height:100%;min-height:100%;resize:none" disabled="true"></textarea>
+				<div id="consoletext" style="width:100%;height:100%;overflow-y:auto"></div>
 			</div>
 		</div>
 		<div id="input" style="width:100%;height:10%;border:all=1px;border-style:solid;display:inline-block">
-			<div id="commandline" style="padding:1%">
+			<div id="commandline" style="padding:1%" style="display:none;">
 				<label>Input nao command:</label>
 				<input type="text" id="textinput" style="width:90%"></input>
-				<button type="button" id="submitbutton" style="float:right;width:10%"">
+				<button type="button" id="submitbutton" style="float:right;width:10%" onclick="submit();">
 					Submit
 				</button>
 				<script type="text/javascript">
-					$(function()
-					 	{
-							document.getElementById('submitbutton').onclick =
-								function() 
+				
+								function submit() 
 								{
-									alert('test!');
-								};
-						});
+									var input = $("#textinput").val().trim();
+									if (input == '') return;
+									$.ajax({
+										  type: "POST",
+										  contentType: "application/json; charset=utf-8",
+										  url: "/command",
+										  data: "{\"command\":\""+input+"\"}",
+										  dataType: "text",
+										}).done( function(console_output) { 
+											var console = $("#consoletext")
+											console.append("<pre>" + console_output + "</pre>"); 
+											console.scrollTop(console.prop('scrollHeight'));
+										});
+								}
+							/*2 timers, one for each GET to camera image after we see the naoconnected cookie is = 1
+							/*<img src="data:image/png:base64,' + contents + ' />"*/
 				</script>
+			</div>
 		</div>
 	</body>
 </html>
